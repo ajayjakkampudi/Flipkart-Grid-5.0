b14d1000683b94bd4775b52ebb77c56944686035
unknown <soumyardhal03@gmail.com>
2023-08-20 23:35:39 +0530

    minor fixes

--- flipkart-client/src/components/orderItem/OrderItem.js
+++ flipkart-client/src/components/orderItem/OrderItem.js
@@ -15,7 +15,7 @@ export default function OrderItem({item}) {
   return (
     <div className="order-result-item">
       <img
-        src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
+        src={item.imgUrl}
         alt=""
         className="order-result-img"
       />
